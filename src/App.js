7ac3e8c69161e5e5724a42ed9c6d49f496e79287
Julian <julianrosenthal@yahoo.com>
2021-10-05 13:53:21 -0700

    setup project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { makeStyles } from '@mui/styles'
+import { purple } from '@mui/material/colors'
 
-function App() {
+
+const useStyles = makeStyles(theme => ({
+  app: {
+    backgroundImage: `linear-gradient(95deg, ${purple[500]} 0%, ${purple[200]} 100%)`
+  }
+}))
+
+const App = () => {
+  const classes = useStyles()
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.app}>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
