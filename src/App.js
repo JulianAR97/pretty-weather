70a502b259c6baf9b047f7489e33119a615502a4
Julian <julianrosenthal@yahoo.com>
2021-10-07 11:02:33 -0700

    connected app to store

--- src/App.js
+++ src/App.js
@@ -3,27 +3,53 @@ import { makeStyles } from '@mui/styles'
 import { deepPurple } from '@mui/material/colors'
 import CardContainer from 'components/CardContainer'
 import StyledSwitch from 'components/StyledSwitch'
+import { connect } from 'react-redux'
+import { setTempUnit } from 'actions/appActions'
 
 const useStyles = makeStyles(theme => ({
   app: {
     backgroundImage: `linear-gradient(15deg, ${deepPurple[900]} 0%, ${deepPurple[200]} 100%)`,
     height: '100%',
+    width: '100%',
     padding: '10px',
     display: 'grid',
     placeItems: 'center'
+  },
+
+  switch: {
+    position: 'absolute',
+    top: 10,
   }
 }))
 
-const App = () => {
+const App = (props) => {
   const classes = useStyles()
   
+  const handleSwitch = (e) => {
+    
+    if (e.target.checked) 
+      props.setTempUnit('f')
+    else 
+      props.setTempUnit('c')
+    
+  }
   return (
     <div className={classes.app}>
-      <StyledSwitch />
+      
+      <div className={classes.switch}>
+        <StyledSwitch onChange={handleSwitch} checked={props.tempUnit === 'f'}/>      
+      </div>
+
       <CardContainer />
+
     </div>
   )
 }
 
-export default App
+const mapStateToProps = state => ({
+  tempUnit: state.tempUnit
+})
+
+
+export default connect(mapStateToProps, {setTempUnit})(App)
 
